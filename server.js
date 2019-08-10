276d243eb46144474d5366812fef88d43dbdbd19
cjwast <cj.wast@hotmail.com>
2019-08-10 11:08:08 -0500

    commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:8080']
+  origin: ['https://hardcore-lalande-8009cd.netlify.com']
 }));
 
 /*ROUTES*/
